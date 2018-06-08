5c2aed7c1528a0bcb24d9538c3e33d62d5f597ee
sgm4789 <sgm4789@gmail.com>
2018-06-08 16:52:34 -0500

    added font awesome, and basic navbar

--- waspp-ui/src/App.js
+++ waspp-ui/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import EditUsers from './containers/Edit Users/EditUsers';
 import classes from '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar/Navbar';
 
 //"navbar navbar-expand-sm navbar-light bg-primary"
 
@@ -8,13 +9,7 @@ class App extends Component {
     render() {
       return (
         <div>
-            <nav className="navbar navbar-expand-sm navbar-light bg-primary" >
-                <a className="navbar-brand" style={{color: 'white'}}>Sysco</a>
-                <ul className="navbar-nav ml-auto">
-                    <li><i className="nav-item nav-link fas fa-user" style={{color: 'white', display: 'flex', flexFlow: 'row'}}> User</i></li>
-                    <li><i className="nav-item nav-link fas fa-power-off"  style={{color: 'white', display: 'flex', flexFlow: 'row'}}></i></li>
-                </ul>
-            </nav>
+          <Navbar />
           <h1 style={{textAlign: 'center'}}>Welcome to Syspay</h1>
           <EditUsers/>
         </div>
---
+++ waspp-ui/src/components/Navbar/Navbar.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import classes from '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+const navbar = () =>{
+    return (
+    //     <nav className="navbar navbar-expand-sm navbar-light bg-primary" >
+    //         {/* <a className="navbar-brand" href="#" style={{color: 'white'}}>Sysco</a>
+    //         <ul className="navbar-nav ml-auto">            
+    //             <li><i className="nav-item nav-link fas fa-user" style={{color: 'white', display: 'flex', flexFlow: 'row'}}> User</i></li>
+    //             <li><i className="nav-item nav-link fas fa-power-off"  style={{color: 'white', display: 'flex', flexFlow: 'row'}}></i></li>
+    //         </ul> */}
+    //     </nav>
+
+    <nav className={[classes.navbar, classes['navbar-expand-sm'], classes['navbar-light'], classes['bg-primary']].join(' ')}>
+        <a classes={[classes['navbar-brand']]} href="#" style={{color: 'white'}}>Sysco</a>
+        <ul className={[classes['navbar-nav'], classes['ml-auto']].join(' ')}>
+            <li><i className={[classes['nav-item'], classes['nav-link'], 'fas', 'fa-user'].join(' ')} style={{color: 'white'}}> User</i></li>
+            <li><i className={[classes['nav-item'], classes['nav-link'], 'fas', 'fa-power-off'].join(' ')}  style={{color: 'white'}}></i></li>
+        </ul>
+    </nav>  
+
+        
+
+    //   <button className={[classes.btn, classes['btn-primary']].join(' ')}>dsds</button> 
+        
+
+
+    );
+};
+
+export default navbar;
