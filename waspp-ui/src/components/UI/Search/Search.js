080ce3752587f64175dd4bf28333108febe0bd1f
Junjie Wang <junjie2412@gmail.com>
2018-06-08 15:41:39 -0400

    bootstrap test

--- waspp-ui/src/components/UI/Search/Search.js
+++ waspp-ui/src/components/UI/Search/Search.js
@@ -2,11 +2,19 @@ import React from 'react';
 import classes from './Search.css';
 
 const search = (props) => {
+
+    const options = props.elements.map(option => (
+        <option
+            key={option}>
+            {option}
+        </option>
+    ));
+
     return (
         <div className={classes.Search}>
             <input className={classes.SearchText} type="text" placeholder={props.placeholder}/>
             <select className={classes.SearchSelect}>
-                <option>12345678</option>
+                {options}
             </select>
         </div>
     );
--- waspp-ui/src/containers/Edit Users/EditUsers.js
+++ waspp-ui/src/containers/Edit Users/EditUsers.js
@@ -3,11 +3,17 @@ import Search from '../../components/UI/Search/Search';
 import Graph from '../../components/UI/Graph/Graph';
 
 class EditUsers extends Component {
+
+    state = {
+        elements: [],
+        searchBy: ['AS400 ID', 'Active Directory', 'Payroll Number']
+    }
+
     render() {
         return (
             <div>
                 <h1>Users List</h1>
-                <Search placeholder="users"/>
+                <Search placeholder="users" elements={this.state.searchBy}/>
                 <Graph/>
             </div>
         );
