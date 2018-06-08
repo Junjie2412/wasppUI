391cefaaec7dc6196be13dec408f921c989ff05a
Junjie Wang <junjie2412@gmail.com>
2018-06-08 16:29:07 -0400

    bootstrap and css modules testing

--- waspp-ui/src/App.js
+++ waspp-ui/src/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 import EditUsers from './containers/Edit Users/EditUsers';
+import classes from '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+//"navbar navbar-expand-sm navbar-light bg-primary"
 
 class App extends Component {
     render() {
       return (
         <div>
+            <nav className="navbar navbar-expand-sm navbar-light bg-primary" >
+                <a className="navbar-brand" style={{color: 'white'}}>Sysco</a>
+                <ul className="navbar-nav ml-auto">
+                    <li><i className="nav-item nav-link fas fa-user" style={{color: 'white', display: 'flex', flexFlow: 'row'}}> User</i></li>
+                    <li><i className="nav-item nav-link fas fa-power-off"  style={{color: 'white', display: 'flex', flexFlow: 'row'}}></i></li>
+                </ul>
+            </nav>
           <h1 style={{textAlign: 'center'}}>Welcome to Syspay</h1>
           <EditUsers/>
         </div>
