885072a51a0314be0eade63403d9a7696a69ff9a
Junjie Wang <junjie2412@gmail.com>
2018-06-11 11:37:44 -0400

    imported axios, testing fake data

--- waspp-ui/src/containers/Edit Users/EditUsers.js
+++ waspp-ui/src/containers/Edit Users/EditUsers.js
@@ -1,24 +1,31 @@
 import React, {Component} from 'react';
 import Search from '../../components/UI/Search/Search';
 import Graph from '../../components/UI/Graph/Graph';
+import * as Links from '../../shared/databaseHttpLinks';
+import axios from 'axios';
 
 class EditUsers extends Component {
 
     state = {
-        elements: [],
         searchBy: ['AS400 ID', 'Active Directory', 'Payroll Number'],
         AS400List: ['AS4001', 'AS4002', 'AS4003', 'AS4004'],
         ADList: ['AD1', 'AD2', 'AD3', 'AD4'],
         PayrollList: ['Payroll1', 'Payroll2', 'Payroll3', 'Payroll4'],
         datalist: ['AS4001', 'AS4002', 'AS4003', 'AS4004'],
-        text: '',
+        userLookup: '',
         placeholder: 'AS400 ID'
     }
 
-    onChangeText = (event) => {
-        this.setState({text: event.target.value});
+    componentDidMount() {
+        console.log(axios.get(Links.EDIT_USER_DB))
     }
 
+    //This handler changes what the value property is whenever we change the search input text
+    onChangeText = (event) => {
+        this.setState({userLookup: event.target.value});
+    }
+
+    //This handler changes the state properties based on which value was selected
     onChangeSelect = (event) => {
         this.setState({text: '', placeholder: event.target.value});
         switch(event.target.value){
@@ -42,7 +49,7 @@ class EditUsers extends Component {
                     options={this.state.searchBy}
                     dataList={this.state.datalist}
                     change={(event) => this.onChangeSelect(event)}
-                    value={this.state.text}
+                    value={this.state.userLookup}
                     changeText={(event) => this.onChangeText(event)}/>
                 <Graph/>
             </div>
