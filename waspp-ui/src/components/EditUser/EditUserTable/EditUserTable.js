ebff6c0d7094ef7c2967b259bcdf29ef63381c9b
sgm4789 <sgm4789@gmail.com>
2018-06-12 10:25:09 -0500

    Added edit user table, fixed nabar scrolling, minor changes

---
+++ waspp-ui/src/components/EditUser/EditUserTable/EditUserTable.js
@@ -0,0 +1,25 @@
+import React, {component} from 'react';
+import Table from '../../Table/Table';
+import classes from '../../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+
+const editUserTable = () =>{
+    return (
+        <div className={classes['col-sm-5']}>
+            <div className={classes.card}>
+                <div className={[classes['card-body'], classes['shadow-lg']].join(' ')}>
+                    <Table />
+                    <div className={classes.row}>
+                        <div className={classes['col-sm-12']}>
+                            <button className={[classes.btn, classes['btn-success'], classes['col-sm-2']].join(' ')}>Add</button>
+                            <button className={[classes.btn, classes['btn-primary'], classes['col-sm-2']].join(' ')} style={{margin: '4px'}}>Update</button>
+                            <button className={[classes.btn, classes['btn-danger'], classes['col-sm-2']].join(' ')}>Delete</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default editUserTable;
--- waspp-ui/src/components/Navbar/Navbar.js
+++ waspp-ui/src/components/Navbar/Navbar.js
@@ -4,7 +4,7 @@ import classes from '../../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 const navbar = () =>{
     return (
 
-    <nav className={[classes.navbar, classes['navbar-expand-sm'], classes['navbar-light'], classes['bg-primary']].join(' ')}>
+    <nav className={[classes.navbar, classes['navbar-expand-sm'], classes['navbar-light'], classes['bg-primary'], classes['fixed-top']].join(' ')}>
         <a classes={[classes['navbar-brand']]} href="/" style={{color: 'white'}}>Sysco</a>
         <ul className={[classes['navbar-nav'], classes['ml-auto']].join(' ')}>
             <li><i className={[classes['nav-item'], classes['nav-link'], 'fas', 'fa-user'].join(' ')} style={{color: 'white'}}> User</i></li>
