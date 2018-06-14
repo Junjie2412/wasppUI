204cc4671614a58b546edf8fe3e8d8c3da2cd061
sgm4789 <sgm4789@gmail.com>
2018-06-14 16:23:02 -0500

    implementing best practices, uniform variable names

--- waspp-ui/src/components/EditUser/User/User.js
+++ waspp-ui/src/components/EditUser/User/User.js
@@ -19,26 +19,26 @@ class user extends Component{
     render(){
         return(
             <div style={{width: '25%', margin: '3%'}}>
-                <div className={[[classes.card], classes['shadow-lg']].join(' ')} style={{backgroundColor:'#0099cc'}}>
-                    <div className={[classes['card-body']]}>
-                        <h5 className={[classes['text-center'], classes['card-title']].join(' ')} style={{color:'#fff'}}>First Last</h5>
+                <div className={[[bootStrapClasses.card], bootStrapClasses['shadow-lg']].join(' ')} style={{backgroundColor:'#0099cc'}}>
+                    <div className={[bootStrapClasses['card-body']]}>
+                        <h5 className={[bootStrapClasses['text-center'], bootStrapClasses['card-title']].join(' ')} style={{color:'#fff'}}>First Last</h5>
                         <p style={{color:'#fff'}}>AS400 ID: {this.props.user.AS400ID}</p>
                         <p style={{color:'#fff'}}>HR Employee ID: {this.props.user.PayrollNumber}</p>
                         <p style={{color:'#fff'}}>File Number: {this.props.user.FileNumber}</p>
                         <p style={{color:'#fff'}}>Weekly Base: {this.props.user.Base}</p>
                         <p style={{color:'#fff'}}>Bonus Flight: </p>
-                        <div className={classes.row}>
-                            <div className = {classes['col-sm-4']}>
-                                <input type='checkbox' className={classes['form-check-label']}/>
+                        <div className={bootStrapClasses.row}>
+                            <div className = {bootStrapClasses['col-sm-4']}>
+                                <input type='checkbox' className={bootStrapClasses['form-check-label']}/>
                                 <label className='form-check-label' style={{color:'#fff'}}>Seasonal</label>
                             </div>
-                            <div className = {classes['col-sm-4']}>
-                                <input type='checkbox' className={classes['form-check-label']}/>
+                            <div className = {bootStrapClasses['col-sm-4']}>
+                                <input type='checkbox' className={bootStrapClasses['form-check-label']}/>
                                 <label className='form-check-label' style={{color:'#fff'}}>Bonus</label>
                             </div>
                         </div>
-                        <div className={[classes['col-sm-12']]}>
-                            <button onClick={this.editUserModal} className={[classes.btn, classes['btn-primary'], classes['offset-sm-3']].join(' ')} style={{backgroundColor:'#006699'}}>Edit Employee</button>
+                        <div className={[bootStrapClasses['col-sm-12']]}>
+                            <button onClick={this.editUserModal} className={[bootStrapClasses.btn, bootStrapClasses['btn-primary'], bootStrapClasses['offset-sm-3']].join(' ')} style={{backgroundColor:'#006699'}}>Edit Employee</button>
                             <Modal show={this.state.modalShow} modalClosed={this.closeModal}>
                                 <EditEmployeeForm user={this.props.user}/>
                             </Modal>
