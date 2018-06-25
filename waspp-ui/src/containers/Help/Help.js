764f121ae5802a2fb53229916dc31b6bb4473c45
Junjie Wang <junjie2412@gmail.com>
2018-06-25 10:23:13 -0400

    inline to css class

--- waspp-ui/src/containers/Help/Help.js
+++ waspp-ui/src/containers/Help/Help.js
@@ -1,17 +1,18 @@
 import React, {Component} from 'react';
 import FrequentIssues from './FrequentIssues/FrequentIssues';
 import { Issues } from '../../shared/Issues';
+import classes from './Help.css';
 
 class Help extends Component {
 
     render() {
       
         return (
-            <div style={{height: '100%'}}>
-                <h1 style={{textAlign: 'center', marginTop: '20px'}}>
+            <div className={classes.Help}>
+                <h1 className={classes.Header}>
                     Help
                 </h1>
-                <div style={{overflow: 'auto', height: '81%', width: '100%'}}>
+                <div className={classes.FAQ}>
                     <FrequentIssues issues={Issues}/>
                 </div>
             </div>
