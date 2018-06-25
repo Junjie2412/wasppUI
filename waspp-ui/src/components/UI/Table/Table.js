74cc96b9c7e2b659d1d401b7afbc1e35afc1248e
Junjie Wang <junjie2412@gmail.com>
2018-06-25 10:08:56 -0400

    changed inputs on tables'

--- waspp-ui/src/components/UI/Table/Table.js
+++ waspp-ui/src/components/UI/Table/Table.js
@@ -4,17 +4,24 @@ import classes from './Table.css';
 import Aux from '../../../hoc/Auxiliary/Auxiliary';
 import * as actions from '../../../store/actions/index';
 import {connect} from 'react-redux';
+import DateInput from '../../UI/DateInput/DateInput';
 
-const table = (props) =>{
+const table = (props) => {
+
+    let show=true;
+
+    const click=(show) => {
+        show=!show;
+    }
 
     const tableData = props.tableData.map(data => (
         <tr key={data.id}
             className={props.isAfter ? (data.id===props.selectedAfterFloorAdjustment.id ? classes.Selected : classes.TableTr)
                 : (data.id===props.selectedAdjustment.id ? classes.Selected : classes.TableTr)}
             onClick={props.isAfter ? ((adj) => props.onSelectAfterFloorAdjustment(data)):((adj) => props.onSelectAdjustment(data))}>
-            <td>{data.amount}</td>
+            <td><div><input value={data.amount} style={{border: 'none'}}/></div></td>
             <td>{data.weekEndDate}</td>
-            <td>{data.comment}</td>
+            <td><input value={data.comment} className={classes.tdInput}/></td>
         </tr>
     ));
     return(
