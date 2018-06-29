dc7647847e9d42567a5888be1393ac1a15bd1bea
Junjie Wang <junjie2412@gmail.com>
2018-06-29 14:38:18 -0400

    new things

--- waspp-ui/src/components/Banner/Banner.js
+++ waspp-ui/src/components/Banner/Banner.js
@@ -2,7 +2,7 @@ import React from 'react';
 import bootStrapClasses from '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import {NavLink} from 'react-router-dom';
 import classes from './Banner.css';
-import logo from '../../assets/SPPay_simple_white2.png';
+import logo from '../../assets/SPPay_simple_white_outlined.png';
 
 const navbar = (props) =>{
 
